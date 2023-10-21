61cbaca083c0483f63817934c15439525eddcc6c
pinokio2003 <pinokio0476@gmail.com>
2023-10-21 19:18:00 +0300

    big changes (3 rectangle + Image)

--- RelaxForIvan/ContentView.swift
+++ RelaxForIvan/ContentView.swift
@@ -18,20 +18,67 @@ struct ContentView: View {
                 .frame(width: 300, height: 300)
                 .foregroundColor(.white)
                 .shadow(color: .black.opacity(0.1), radius: 20, x: 0, y: 0)
-            
+
             RoundedRectangle(cornerRadius: 20, style: .continuous)
                 .frame(width: 250, height: 250)
                 .foregroundColor(Color("G1"))
                 .offset(x: motion != nil ? CGFloat(motion!.gravity.x * 20) : 0,
                         y: motion != nil ? CGFloat(motion!.gravity.y * 20) : 0)
-            
                 .rotation3DEffect(motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0), axis: (
-                                        x: motion != nil ? -motion!.gravity.y : 0,
-                                        y: motion != nil ? motion!.gravity.x : 0,
-                                        z: 0))
-                .shadow(color: .black.opacity(0.3), radius: 3,
-                        x: motion != nil ? CGFloat(-motion!.gravity.y * 3) : 0,
-                        y: motion != nil ? CGFloat(motion!.gravity.x * 3) : 0)
+                    x: motion != nil ? -motion!.gravity.y : 0,
+                    y: motion != nil ? motion!.gravity.x : 0,
+                    z: 0))
+                .shadow(color: .black.opacity(0.3), radius: 1,
+                        x: motion != nil ? CGFloat(-motion!.gravity.x * 3) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 3) : 0)
+            //New rectangle
+            
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 200, height: 200)
+                .foregroundColor(Color("G2"))
+                .offset(x: motion != nil ? CGFloat(motion!.gravity.x * 27) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 27) : 0)
+                .rotation3DEffect(motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0), axis: (
+                    x: motion != nil ? -motion!.gravity.y : 0,
+                    y: motion != nil ? motion!.gravity.x : 0,
+                    z: 0))
+                .shadow(color: .black.opacity(0.35), radius: 1,
+                        x: motion != nil ? CGFloat(-motion!.gravity.x * 5) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 5) : 0)
+            
+            //Enother one rectangle
+            
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 165, height: 165)
+                .foregroundColor(Color("G3"))
+                .offset(x: motion != nil ? CGFloat(motion!.gravity.x * 40) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 40) : 0)
+                .rotation3DEffect(motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0), axis: (
+                    x: motion != nil ? -motion!.gravity.y : 0,
+                    y: motion != nil ? motion!.gravity.x : 0,
+                    z: 0))
+                .shadow(color: .black.opacity(0.5), radius: 1,
+                        x: motion != nil ? CGFloat(-motion!.gravity.x * 7) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 7) : 0)
+            
+            //Image
+            
+                .overlay(alignment: .center) {
+                    Image("LogoOne")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 140, height: 140)
+                }
+                .offset(x: motion != nil ? CGFloat(motion!.gravity.x * 40) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 40) : 0)
+                .rotation3DEffect(motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0), axis: (
+                    x: motion != nil ? -motion!.gravity.y : 0,
+                    y: motion != nil ? motion!.gravity.x : 0,
+                    z: 0))
+                .shadow(color: .black.opacity(0.5), radius: 1,
+                        x: motion != nil ? CGFloat(-motion!.gravity.x * 9) : 0,
+                        y: motion != nil ? CGFloat(motion!.gravity.y * 9) : 0)
+            
         }
         .onAppear {
             if motionManager.isDeviceMotionAvailable {
